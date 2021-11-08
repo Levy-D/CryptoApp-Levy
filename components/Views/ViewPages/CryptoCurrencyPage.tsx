5c475abec469e1184d08040a8a13d75e35723ca3
Levy Daelman <31793568+Levy-D@users.noreply.github.com>
2021-11-08 17:11:25 +0100

    .

--- components/Views/ViewPages/CryptoCurrencyPage.tsx
+++ components/Views/ViewPages/CryptoCurrencyPage.tsx
@@ -1,17 +1,46 @@
-import React, {useContext} from 'react';
-import {Text, View} from 'react-native';
+import React, { useContext } from 'react';
+import {StyleSheet, View, Text } from 'react-native';
 import { DataItemContext } from '../../Helper/Context';
 import Header from '../ViewComponents/Header';
+import SubHeader from '../ViewComponents/SubHeader';
 
-const cryptoCurrencyPage = () => { 
-	const {dataItem} = useContext(DataItemContext);
+const cryptoCurrencyPage = () => {
+	const { dataItem } = useContext(DataItemContext);
 
-	return(
-	<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-		<Header title={dataItem.name}></Header>
-	</View>
+	return (
+		<View style={styles.view}>
+			<Header title={dataItem.name}></Header>
+			<SubHeader title='Statistics'></SubHeader>
+			<Text>Market Cap</Text> 
+			<Text>{dataItem.quote.USD.market_cap.toFixed(2)}</Text>
+			<Text>Volume 24h</Text> 
+			<Text>{dataItem.quote.USD.volume_24h.toFixed(2)}</Text>
+			<Text>Max Supply</Text> 
+			<Text>{dataItem.max_supply.toFixed(2)}</Text>
+			{/* <Text>All Time High</Text> 
+			<Text>{dataItem.quote.USD.market_cap}</Text>
+			<Text>All Time Low</Text> 
+			<Text>{dataItem.quote.USD.market_cap}</Text> */}
+			<Text>Fully Diluted Market Cap</Text> 
+			<Text>{dataItem.quote.USD.fully_diluted_market_cap.toFixed(2)}</Text>
+			<Text>Circulating Supply</Text> 
+			<Text>{dataItem.circulating_supply.toFixed(2)}</Text>
+			<Text>Total Supply</Text> 
+			<Text>{dataItem.total_supply.toFixed(2)}</Text>
+			<Text>Rank</Text> 
+			<Text>#{dataItem.cmc_rank}</Text>
+			<Text>Market Dominance</Text> 
+			<Text>{dataItem.quote.USD.market_cap_dominance.toFixed(2)}%</Text>
+		</View>
 	);
 };
 
+
+const styles = StyleSheet.create({
+	view: {
+		flex: 1,
+		alignItems: 'center',
+	}
+});
 
 export default cryptoCurrencyPage;
