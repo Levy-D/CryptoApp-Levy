e4efc8c5aa1d2c9f7cd9a1f2a0414903648ad136
Levy-D <31793568+Levy-D@users.noreply.github.com>
2021-12-18 00:06:16 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,28 +1,18 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import TabNavigation from './components/Views/Navigation/TabNavigation';
 import {Provider} from 'react-redux';
 import {store, persistor} from './components/Redux/store';
 import {PersistGate} from 'redux-persist/lib/integration/react';
-import SplashScreen from 'react-native-splash-screen';
 
-const App = () => {
-	useEffect(() => {
-		setTimeout(() => {
-			// Timeout
-		}, 5000);
-		SplashScreen.hide();
-	}, []);
-
-	return (
-		<Provider store={store} >
-			<PersistGate loading={null} persistor={persistor}>
-				<NavigationContainer>
-					<TabNavigation />
-				</NavigationContainer>
-			</PersistGate>
-		</Provider>
-	);
-};
+const App = () => (
+	<Provider store={store} >
+		<PersistGate loading={null} persistor={persistor}>
+			<NavigationContainer>
+				<TabNavigation />
+			</NavigationContainer>
+		</PersistGate>
+	</Provider>
+);
 
 export default App;
